d4129c2713ef89cccce5e83c3736d959f68ae326
Frida Kiriakos <kiriakos.frida@gmail.com>
2015-03-19 23:15:33 -0700

    adding the static html file to public folder for easy access

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
