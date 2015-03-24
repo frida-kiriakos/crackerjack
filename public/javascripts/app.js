ab633a7935ac5ccd747c69b1a74284e3b95cffe3
jmovius <jmovius@J-Laptop.local>
2015-03-24 15:57:00 -0700

    Implementing Feathr.it
    Feathr.it API included with project.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,7 +12,6 @@ var main = function () {
 			$p.text(post.body + " - By: " + post.author.username);
 			// replace this with the correct tag ID
 			// $("#pendingTweets").append($p);
-
 		});
 	});
 /* posts are being diplayed in the embedded JS in views/index.ejs
@@ -87,5 +86,59 @@ var main = function () {
 	$('div .input-buttons').append($btn_addpost);
 */
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+	var _maxLength = 250,
+		revertInput = "",
+		inputUrl = "",
+		originalLen = 0,
+		$charLen = $('div .char-len'),
+		$inputText = $('textarea.input-text').attr('maxlength',_maxLength).attr('placeholder',"Input Tweet"),
+		$btn_revert = $("<button>").text("Revert").attr('class', 'btn btn-default').attr('disabled',true),
+		$btn_shorten = $("<button>").text("Shorten").attr('class', 'btn btn-default');
+
+	var setCharLen = function(len) {
+		if(len===0) {
+			$btn_shorten.attr('disabled', true);
+		}
+		$charLen.text(len + '/' + _maxLength);
+	};
+	setCharLen(0);
+
+	$inputText.on('keydown', function(event) {
+		originalLen = $inputText.val().length;
+	});
+
+	$inputText.on('keyup', function(event) {
+		if(originalLen != $inputText.val().length) {
+			setCharLen($inputText.val().length);
+			$btn_revert.attr('disabled', true);
+			$btn_shorten.attr('disabled', false);
+		}
+	});
+
+	$btn_revert.on("click", function () {
+		$inputText.val(revertInput);
+		setCharLen(revertInput.length);
+		$btn_revert.attr('disabled', true);
+		$btn_shorten.attr('disabled', false);
+	});
+
+	$btn_shorten.on("click", function () {
+		$btn_shorten.attr('disabled', true);
+		if($inputText.val().length > 0) {
+			revertInput = $inputText.val();
+
+			inputUrl = "http://www.feathr.it/api?text=" + $inputText.val().replace(/ /g, '%20');
+
+			// Feather.it API Call
+			$.getJSON(inputUrl, function (response) {
+				$inputText.val(response[0].Shorten_Text);
+				setCharLen(response[0].Shorten_Text.length);
+			});
+		}
+		$btn_revert.attr('disabled', false);
+	});
+
+	$('div .input-buttons').append($btn_revert, $btn_shorten);
 };
 $(document).ready(main);
